46bb8ddaef967e4a2543afa8b5774a0965b42381
Pinky-pie <iosif.dr4gutu@gmail.com>
2022-03-07 21:36:29 +0200

    Bug fixed
    Fixed the bug where the payer couldn't looked around anymore

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -5,35 +5,47 @@ using UnityEngine;
 public class OpenDoor : MonoBehaviour
 {
     Animator animator;
+    Animator playerAnimator;
+    bool triggered;
+
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        playerAnimator = GameObject.Find("VrBase").GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (playerAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !playerAnimator.IsInTransition(0))
+        {
+            playerAnimator.enabled = false;
+        }
     }
     public void Activate()
     {
-        animator.SetTrigger("Activate");
-        StartWalking(3);
+        if(!triggered)
+        {
+            animator.SetTrigger("Activate");
+            StartCoroutine(StartWalking(3));
+            triggered = true;
+        }
+      
     }
     IEnumerator StartWalking(float seconds)
     {
         //Print the time of when the function is first called.
-        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+        //Debug.Log("Started Coroutine at timestamp : " + Time.time);
 
         //yield on a new YieldInstruction that waits for 5 seconds.
         yield return new WaitForSeconds(seconds);
 
         //After we have waited 3 seconds print the time again.
-        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
-        var playerAnimator = GameObject.Find("VrBase").GetComponent<Animator>();
+        //Debug.Log("Finished Coroutine at timestamp : " + Time.time);
         playerAnimator.enabled = true;
         playerAnimator.SetTrigger("Play");
+        
     }
 }
